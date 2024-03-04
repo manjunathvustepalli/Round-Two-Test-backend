74c0d47c52dcaa6faec304659d552bb8f49aff93
Majunath Vustepalli <manjunathvustepalli.com>
2024-03-04 15:58:06 -0500

    first draft

--- server.py
+++ server.py
@@ -16,7 +16,6 @@ def upload_csv():
 
     if not file.filename.endswith('.csv'):
         return jsonify({"message": "File is not a CSV"}), 400
-
     file.save("uploads/" + file.filename)
 
     return jsonify({"message": "CSV uploaded successfully"}), 200
